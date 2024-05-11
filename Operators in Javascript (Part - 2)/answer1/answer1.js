92a520875fd823284d2c64e8e1d13ac945980f26
comm-cod <128215889+comm-cod@users.noreply.github.com>
2024-05-11 18:03:20 +0530

    answer files added

---
+++ Operators in Javascript (Part - 2)/answer1/answer1.js
@@ -0,0 +1,4 @@
+const num = 33;
+const result = num % 2 === 0 ? "true" : "false";
+
+console.log(result);
---
+++ Operators in Javascript (Part - 2)/answer3/answer3.js
@@ -0,0 +1,7 @@
+const num = 3;
+
+num > 0 ? console.log("positive") : 
+    num < 0 ? console.log("negative") :
+    console.log("zero");
+
+    //result: positive;
